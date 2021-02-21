5bc2f9610dd786835de23c65ae522776e69a1468
dinuka-rp <drpiyadigama@gmail.com>
2021-02-22 02:44:21 +0530

    recreate xylophone & add functioning audio player

---
+++ xylophone-flutter/xylophone/android/app/src/main/kotlin/lk/dinuka/xylophone/MainActivity.kt
@@ -0,0 +1,6 @@
+package lk.dinuka.xylophone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ xylophone-flutter/xylophone/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+// import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: FlatButton(
+                onPressed: () {
+                  final player = AudioCache(); // player that plays cached audio
+                  player.play(
+                      'note1.wav'); // audio player package already takes sounds from assets/
+                },
+                child: Text('Click Me')),
+          ),
+        ),
+      ),
+    );
+  }
+}
